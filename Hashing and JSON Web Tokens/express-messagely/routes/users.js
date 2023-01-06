8b734f9a6bc7d8371bbee9aff44c42aecfdc131a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-01-06 11:59:37 -0500

    started users GET route

--- Hashing and JSON Web Tokens/express-messagely/routes/users.js
+++ Hashing and JSON Web Tokens/express-messagely/routes/users.js
@@ -2,12 +2,23 @@ const express = require("express");
 const router = express.Router();
 const { SECRET_KEY } = require("../config");
 const User = require("../models/user");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const ExpressError = require("../expressError");
 
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+router.get("/", ensureLoggedIn, async (req, res, next) => {
+	try {
+		const users = await User.all();
+		if (users.length === 0) return next(new ExpressError("No users found!", 404));
+		return { users };
+	} catch (err) {
+		return next(err);
+	}
+});
 
 /** GET /:username - get detail of users.
  *
