4cf15a45533e0ef768c6b06f966625d7b076544d
Jacob Eiferman <jacobeiferman@gmail.com>
2023-01-16 10:40:59 -0500

    users/ GET from messages route

--- Hashing and JSON Web Tokens/express-messagely/routes/users.js
+++ Hashing and JSON Web Tokens/express-messagely/routes/users.js
@@ -44,6 +44,15 @@ router.get("/:username", ensureLoggedIn, async (req, res, next) => {
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get("/:username/to", ensureCorrectUser, async (req, res, next) => {
+	try {
+		const user_messages = await User.messagesTo(req.params.username);
+		if (user_messages.length === 0) return next(new ExpressError("Messages not found!", 404));
+		return res.json({ user_messages });
+	} catch (err) {
+		return next(err);
+	}
+});
 
 /** GET /:username/from - get messages from user
  *
@@ -54,4 +63,13 @@ router.get("/:username", ensureLoggedIn, async (req, res, next) => {
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get("/:username/to", ensureCorrectUser, async (req, res, next) => {
+	try {
+		const user_messages = await User.messagesTo(req.params.username);
+		if (user_messages.length === 0) return next(new ExpressError("Messages not found!", 404));
+		return res.json({ user_messages });
+	} catch (err) {
+		return next(err);
+	}
+});
 module.exports = router;
